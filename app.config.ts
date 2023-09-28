d9d8cd297ae682ccce2195348605ed94dee7419f
김태웅 <rlaxodnd95@gmail.com>
2023-09-28 21:19:30 +0900

    :tada:

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+    title: 'Chulbong',
+    theme: {
+        dark: false,
+        colors: {
+            primary: '#1F41BB',
+            black: '#000000',
+            white: '#FFFFFF',
+            gray: '#D9D9D9',
+            gray2: '#B1B1B1',
+        }
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#1F41BB',
+        black: '#000000',
+        white: '#ffffff',
+        gray: '#D9D9D9',
+        gray2: '#B1B1B1',
+      }
+    },
+  },
+  plugins: [],
+}
+
